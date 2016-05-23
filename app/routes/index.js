3eb4ffeffce0b6c640d603cf221df790572e4ce5
Rob Quick <rob.quick@gmail.com>
2016-05-22 20:04:40 -0400

    setup index model hook

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,23 @@
 import Ember from 'ember';
 
+const { RSVP } = Ember;
+
 export default Ember.Route.extend({
+    
     session: Ember.inject.service(),
+    
     beforeModel() {
         let session = this.get("session");
         if (!session.get("isActive")) {
             this.transitionTo("login");
         }
+    },
+    
+    model() {
+        return RSVP.hash({
+            users: this.store.findAll("user"),
+            messages: this.store.findAll("message")
+        });
     }
+    
 });
