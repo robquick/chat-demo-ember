f2f10a6a1ce155086b06703271a2042963c3c7b0
Rob Quick <rob.quick@gmail.com>
2016-05-24 23:53:16 -0400

    login form triggers save action

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    userName: null,
+    
     didInsertElement() {
         this._super(...arguments);
         this.$("input").focus();
+    },
+    
+    actions: {
+        save() {
+            const userName = this.get("userName");
+            console.log(userName);
+        }
     }
 });
