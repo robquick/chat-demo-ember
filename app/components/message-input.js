194b37f54271f1dee0d101110a91a6eb9ede7922
Rob Quick <rob.quick@gmail.com>
2016-05-25 23:26:33 -0400

    message input form triggers save action

--- app/components/message-input.js
+++ app/components/message-input.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    messageText: null,
+    
     didInsertElement() {
         this._super(...arguments);
         this.$("input").focus();
+    },
+    
+    actions: {
+        save() {
+            const msgText = this.get("messageText");
+            console.log(msgText);
+        }
     }
 });
