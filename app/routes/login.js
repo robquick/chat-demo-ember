6ea05a78da19f28519d489f881437936434a90c9
Rob Quick <rob.quick@gmail.com>
2016-05-28 21:51:39 -0400

    moved user creation to route; added ember-route-action-helper dependency

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    session: Ember.inject.service(),
+    actions: {
+        loginUser(name) {
+            const user = this.store.createRecord("user", { name });
+            user.save().then(u => {
+                this.get("session").start(u);
+                u.ref().onDisconnect().remove();
+                this.transitionTo("index");
+            });
+        }
+    }
 });
