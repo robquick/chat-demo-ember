8b922f5270dbec846b5d45c27e7883f7a4ce2d34
Rob Quick <rob.quick@gmail.com>
2016-06-05 11:17:45 -0400

    message list now scrolls with new messages

--- app/components/message-list.js
+++ app/components/message-list.js
@@ -1,5 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNames: ["message-list"]
+    classNames: ["message-list"],
+    shouldScrollToBottom: false,
+    
+    scrollToBottom() {
+        const scrollHeight = this.$()[0].scrollHeight;
+        this.$().scrollTop(scrollHeight);
+    },
+    
+    didInsertElement() {
+        this._super(...arguments);
+        this.scrollToBottom();
+    },
+    
+    willUpdate() {
+        const div = this.$();
+        const shouldScroll = (div.scrollTop() + div.height()) === div[0].scrollHeight;
+        this.set("shouldScrollToBottom", shouldScroll);
+    },
+    
+    didUpdate() {
+        this._super(...arguments);
+        if (this.get("shouldScrollToBottom")) {
+            this.scrollToBottom();
+        }
+    }
 });
